5b604a3dcbbe58f123230de3a935dc6d0754bd24
kushagramutreja05 <kushagramutreja05@gmail.com>
2024-12-06 09:22:24 +0530

    Final Model

--- app.py
+++ app.py
@@ -11,13 +11,6 @@ import keras
 from keras.layers import Multiply, Dense, Lambda
 from keras import backend as K
 
-
-
-
-
-
-
-
 app = Flask(__name__)
 CORS(app)
 
@@ -29,7 +22,7 @@ gru_model = load_model(gru_model_path,custom_objects={
    
     'tf':tf
     
-},safe_mode=True)
+},safe_mode=False)
 
 # Load the linear regression model
 linear_model_path = 'linear_regression_model.pkl'
@@ -44,7 +37,26 @@ with open(scaler_x_path, 'rb') as file:
 scaler_y_path = 'model/scaler_y.pkl'
 with open(scaler_y_path, 'rb') as file:
     scaler_y = pickle.load(file)
+print("Original data_max_:", scaler_x.data_max_)
+print("Original data_min_:", scaler_x.data_min_)
+print("Original scale_:", scaler_x.scale_)
 
+custom_max = scaler_x.data_max_.copy()
+custom_maxy=scaler_y.data_max_.copy()
+# Modify only the data_max_
+custom_max[0] = 80234.08  # New maximum value for Open Price
+custom_max[3] = 17100  # New maximum value for Volume
+custom_max[4] = 80415.47 
+custom_max[5] = 80511.15
+custom_max[6] = 79844.79
+scaler_x.data_max_ = custom_max
+
+# Recompute scale_ based on the new max value and existing min value
+scaler_x.scale_ = 2 / (scaler_x.data_max_ - scaler_x.data_min_)
+
+custom_maxy[0]=80415.49
+scaler_y.data_max_ = custom_maxy
+scaler_y.scale_ = 2 / (scaler_y.data_max_ - scaler_y.data_min_)
 # Initialize VADER sentiment analyzer
 sid = SentimentIntensityAnalyzer()
 
@@ -76,11 +88,13 @@ def predict():
         # Prepare the feature array
         features = np.array([[close_price, compound, compound_shifted, volume, open_price, high, low]])
         print("Features before scaling:", type(features))
-        features_scaled=pd.DataFrame(features)
+        
         # Min-max scale the features
-        features_scaled = scaler_x.transform(features_scaled)  # Use transform, not fit_transform
+        features_scaled = scaler_x.transform(features)  # Use transform, not fit_transform
+        print(type(features_scaled))
         features_scaled = features_scaled.reshape(features_scaled.shape + (1,))
        
+        
         # Predict next day's close price using the GRU model
         gru_prediction = gru_model.predict(features_scaled)
 
@@ -88,7 +102,7 @@ def predict():
         features_scaled = features_scaled.reshape(features_scaled.shape[0], features_scaled.shape[1])
         gru_prediction=gru_prediction.reshape(gru_prediction.shape[0], gru_prediction.shape[1])
         # Combine the GRU prediction with the scaled features
-        combined_features = pd.concat([features_scaled, gru_prediction], axis=1)
+        combined_features = np.hstack((features_scaled, gru_prediction))
 
         # Predict using the linear regression model
         lr_prediction = linear_model.predict(combined_features)
